7adef64fba2efd804ab78c3f000a8d746e6d08ee
abdelrahmanemadismail <abdelrahman.e.ismail@gmail.com>
2025-02-16 19:54:00 +0200

    add env url

--- src/actions/auth-actions.ts
+++ src/actions/auth-actions.ts
@@ -11,7 +11,7 @@ export async function googleLogin() {
   const res = await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo: 'http://localhost:3000/auth/callback',
+      redirectTo: `${process.env.NEXT_PUBLIC_URL}/auth/callback`,
     },
   })
   if (res.data) {
