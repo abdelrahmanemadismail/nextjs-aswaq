67428a937ab0cd30b1ba2f63efe24d88bf0b302c
abdelrahmanemadismail <abdelrahman.e.ismail@gmail.com>
2025-03-14 01:38:17 +0200

    fix incress body size limit

--- src/app/api/webhooks/messages/route.ts
+++
@@ -1,6 +0,0 @@
-// app/api/webhooks/messages/route.ts
-import { processNewMessageWebhook } from '@/utils/webhook-notification-service'
-
-export async function POST(request: Request) {
-  return processNewMessageWebhook(request)
-}
